c048241c978d52a787b46d2c5837885f909f39bb
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-26 14:21:54 +0000

    Wire up SignUpForm

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import { ROOT_URL } from '@env';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { Input, Button } from 'react-native-elements';
+import axios from 'axios';
 
 const SignUpForm = () => {
+  const [phone, setPhone] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone });
+      console.log(response);
+
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+        phone,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View>
       <View>
-        <Input label='Enter Phone Number' />
+        <Input
+          label='Enter Phone Number'
+          value={phone}
+          onChangeText={(text) => setPhone(text)}
+        />
       </View>
-      <Button title='Submit' />
+      <Button title='Submit' onPress={handleSubmit} />
     </View>
   );
 };
